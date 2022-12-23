43fee2b1325a7b343fc327028f4761d28a23dbc1
Alisher <35801338+4hseproject@users.noreply.github.com>
2022-12-23 03:20:27 +0300

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -1,1 +1,10 @@
 #code
+from model import predict
+from conf import settings
+from conf import logging
+
+
+#user is asked to choose which model he wants to use for predictions
+m_num = int(input())
+prediction = predict(settings.PREDICTION.VALUES, m_num)
+logging.info(f"prediction:{prediction}")
